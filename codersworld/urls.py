8c1f4d5a19574f634db4d78007dfdb4bd7ff85f1
sandesh <sldinkar@gmail.com>
2020-09-04 14:21:26 +0530

    Added newspage and links

--- codersworld/urls.py
+++ codersworld/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 import homepage.views
 import news.views
 urlpatterns = [
-    path('news/', news.views.news, name="news"),
     path('', homepage.views.homepage, name="homepage"),
+    path('news/', news.views.news, name="news"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
